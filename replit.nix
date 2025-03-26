cd6b21e9b21b49ce8cb777c1fafe538e6f2de465
Annie-67 <162158116+Annie-67@users.noreply.github.com>
2025-03-26 11:36:36 +0530

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,21 @@
+{pkgs}: {
+  deps = [
+    pkgs.ffmpeg-full
+    pkgs.libsndfile
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.glibcLocales
+    pkgs.tesseract
+    pkgs.libGLU
+    pkgs.libGL
+  ];
+}
